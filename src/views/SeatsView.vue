97d41fe7ff5801ecebaa7dd91488a1b89f4aa2e5
Solmyr77 <91564294+Solmyr77@users.noreply.github.com>
2024-03-20 01:37:44 +0100

    Add files via upload

--- src/views/SeatsView.vue
+++ src/views/SeatsView.vue
@@ -20,12 +20,32 @@ export default {
 
 <template>
     <div class="flex flex-col justify-center items-center w-vw-full h-1 min-h-screen bg-ui-background">
-        <div class="flex justify-center items-center basis-11/12 h-full w-full">
+        <div class="flex flex-col justify-center items-center basis-11/12 h-full w-full">
+            <div v-for="row in seatRows" class="flex w-full h-full justify-around items-center">
+                <div v-for="column in seatColumns" class="bg-ui-card ring-ui-ring ring-1 w-8 h-8">
 
+                </div>
+            </div>
         </div>
 
-        <div class="flex justify-center items-center basis-1/12 h-full w-full">
+        <div class="flex justify-center items-center basis-1/12 h-full w-full bg-ui-background">
+            <div class="flex w-full h-full justify-center items-center">
+                <button class="px-8 py-3 bg-ui-card rounded-lg ring-ui-ring ring-1">
+                    <h1 class="text-white">asd</h1>
+                </button>
+            </div>
 
+            <div class="flex w-full h-full justify-center items-center">
+                <button class="px-8 py-3 bg-ui-card rounded-lg ring-ui-ring ring-1">
+                    <h1 class="text-white">asd</h1>
+                </button>
+            </div>
+
+            <div class="flex w-full h-full justify-center items-center">
+                <button class="px-8 py-3 bg-ui-card rounded-lg ring-ui-ring ring-1">
+                    <h1 class="text-white">asd</h1>
+                </button>
+            </div>
         </div>
     </div>
 </template>
